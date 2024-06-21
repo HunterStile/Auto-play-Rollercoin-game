6310ff2552d6502c70a9c95ed9d42bdd3a7c5e89
HunterStile <you@example.com>
2024-06-21 02:47:38 +0200

    aggiornata la routine

--- Routine.py
+++ Routine.py
@@ -10,10 +10,10 @@ while True:
     while cooldown == True:
         print("In attesa che il gioco sia pronto...")
         sleep(10)
-        muovi_mouse(592,688)                                                #Posiziono il mouse su play
+        muovi_mouse(599,572)                                                #Posiziono il mouse su play
         screenshot_before = pyautogui.screenshot()                          #Salvo lo screenshot prima del click
         #Inizio del gioco
-        click(592,688)                                                      #Click su play
+        click(599,572)                                                      #Click su play
         sleep(2)
         screenshot_after = pyautogui.screenshot()                           #Salvo lo screenshot dopo il click
         cooldown = verifica_cambio(screenshot_before, screenshot_after)     #Verifico se il click ha causato cambiamenti
@@ -23,10 +23,12 @@ while True:
     click(992,438)     #Click per inizia
     sleep(5)
     coinclick(1)
-
+    sleep(3)
     click(970, 678)  #Gain Power
     sleep(3)
-    click(112, 292)  #Choose Game
+    click(960, 637)  #Nel caso si perde 
+    sleep(2)
+    click(30, 290)  #Choose Game
     sleep(3)
 
     #ROUTINE 2048
@@ -37,10 +39,10 @@ while True:
     while cooldown == True:
         print("In attesa che il gioco sia pronto...")
         sleep(10)
-        muovi_mouse(582,986)                                                #Posiziono il mouse su play
+        muovi_mouse(590, 878)                                                #Posiziono il mouse su play
         screenshot_before = pyautogui.screenshot()                          #Salvo lo screenshot prima del click
         #Inizio del gioco
-        click(582,986)                                                      #Click su play
+        click(590, 878)                                                    #Click su play
         sleep(2)
         screenshot_after = pyautogui.screenshot()                           #Salvo lo screenshot dopo il click
         cooldown = verifica_cambio(screenshot_before, screenshot_after)     #Verifico se il click ha causato cambiamenti
@@ -48,10 +50,12 @@ while True:
     #gioco pronto, inizio a giocare
     print("Il gioco è pronto, inizio a giocare...")
     click(1000,500)     #Click per iniziare
-    sleep(5)
+    sleep(4)
     Game2048()
-
+    sleep(3)
     click(970, 678)  #Gain Power
     sleep(3)
-    click(112, 292)  #Choose Game
+    click(960, 637)  #Nel caso si perde 
+    sleep(2)
+    click(30, 290)  #Choose Game
     sleep(3)
